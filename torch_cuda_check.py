b5e1d0c34ca7ab10b0a1413478022bc44ceb349e
renhaofan <yqykrhf@163.com>
2023-03-16 14:44:18 +0800

    refacotr: add more info

--- torch_cuda_check.py
+++ torch_cuda_check.py
@@ -1,11 +1,21 @@
+"""
+Simple script to get the cuda status of pytorch
+# https://pytorch.org/docs/stable/backends.html
+"""
 import torch
-print(torch.cuda.is_available())
-print(torch.backends.cuda.is_built())
-print(torch.backends.cudnn.version())
-print(torch.backends.cudnn.is_available())
-print(torch.backends.cudnn.enabled)
-print('=====================================')
+import os
+
+print(f'PyTorch binary were run a machine \n \
+with working CUDA drivers and devices: {torch.backends.cuda.is_built()}\n')
+
+print(f'CUDA is available: {torch.cuda.is_available()}')
+print(f'cudnn is available: {torch.backends.cudnn.is_available()}')
+print(f'Enable cudnn: {torch.backends.cudnn.enabled}\n')
+
 print(f'Pytorch version {torch.__version__}')
 print(f'Pytorch CUDA {torch.version.cuda}')
-print(f'Pytorch cudnn {torch.backends.cudnn.version()}')
-print('=====================================')
+print(f'Pytorch cudnn {torch.backends.cudnn.version()}\n')
+
+print(f'{torch.cuda.device_count()} CUDA devices available')
+for i in range(torch.cuda.device_count()):
+    print(f'{torch.cuda.get_device_name(i)}')
